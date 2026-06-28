6ae2a53e1052775bf8caf3e2f8696a836908db39
Bet4 <0xbet4@gmail.com>
2026-06-28 12:34:22 +0000

    Remove runc container state workaround

--- distro/etc_nixos/overlays/podman/default.nix
+++ distro/etc_nixos/overlays/podman/default.nix
@@ -1,7 +1,6 @@
 final: prev: {
   runc = prev.runc.overrideAttrs (oldAttrs: {
     patches = (oldAttrs.patches or [ ]) ++ [
-      ./runc-Disable-container-state-check.patch
       ./runc-Disable-creating-dev-mqueue.patch
       ./runc-Disable-eBPF-for-device-filtering.patch
       ./runc-Disable-user-and-capability-setup-checks.patch
