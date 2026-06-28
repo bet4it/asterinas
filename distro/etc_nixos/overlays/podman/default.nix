a81aa3b0526a3542ac4b39e3d45bbe33ebe93994
Bet4 <0xbet4@gmail.com>
2026-06-28 16:13:51 +0000

    Format Podman overlay

--- distro/etc_nixos/overlays/podman/default.nix
+++ distro/etc_nixos/overlays/podman/default.nix
@@ -1,8 +1,8 @@
 final: prev: {
-  runc = prev.runc.overrideAttrs (oldAttrs: {
-    patches = oldAttrs.patches or [ ];
-  });
-  podman = (prev.podman.overrideAttrs (oldAttrs: {
-    patches = oldAttrs.patches or [ ];
-  })).override { runc = final.runc; };
+  runc =
+    prev.runc.overrideAttrs (oldAttrs: { patches = oldAttrs.patches or [ ]; });
+  podman = (prev.podman.overrideAttrs
+    (oldAttrs: { patches = oldAttrs.patches or [ ]; })).override {
+      runc = final.runc;
+    };
 }
